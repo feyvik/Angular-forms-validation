201815c3b235f5a66f4e6000285a29658684aa7c
favour vivian woka <vivianemma97@gmail.com>
2020-03-23 18:32:02 +0100

    form validation

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, NgbModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
